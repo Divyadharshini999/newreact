bf1c5975767bb5995caadb97bbc82799013dd113
Divyadharshini999 <dharshini999dd@gmail.com>
2023-07-13 17:02:36 +0530

    app.js second commit

---
+++ src/components/AddNew.js
@@ -0,0 +1,48 @@
+import React ,{useState}from "react";
+ function AddNew()
+{
+
+    const [Name,setName]= useState("")
+    const [Experience,setExperience]=useState("")
+    const [doj,setDoJ]= useState("")
+    const [emp,setEmp]= useState([{Name:"divyasrii",Experience:"2 years",doj:"22.06.2020"},{
+        
+        Name: "Divya",
+        Experience:"2 Years",
+        doj:"22.06.2020"
+    },
+   
+    
+    
+   ])
+    const HandleAdd=()=>{
+        // console.log(Name,Experience,doj)
+        setEmp([...emp,{Name:Name,Experience:Experience,doj:doj}])
+        setName("");setExperience("");setDoJ("")
+}
+
+    return(
+        <div className="inputContainer">
+            <input type="text" placeholder="Enetr Name" value={Name} onChange={e=>setName(e.target.value)}></input><br/>
+            <input type="text" placeholder="Experience in years" value={Experience} onChange={e=>setExperience(e.target.value)}></input><br/>
+            <input type="text" placeholder="Enter the Date of Joining"value={doj} onChange={e=>setDoJ(e.target.value)}></input><br/>
+            <button type="submit" onClick={HandleAdd}>ADD</button><br></br>
+        
+    
+    
+     {emp.map(e=>(
+        <div className="tableContainer">
+            <table border="1"  cellPadding={1}>
+                <tr>
+            <td>{e.Name}</td> 
+            <td>{e.Experience}</td>
+            <td> {e.doj}</td>
+            </tr>
+            </table>
+            </div>
+            ))} 
+            
+    </div>)
+}
+
+export default AddNew
