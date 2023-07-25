cea822fa3898c9363325b92aad98a5ebb7702063
Divyadharshini999 <dharshini999dd@gmail.com>
2023-07-26 03:20:06 +0530

    sixth commit- update

--- src/components/Update_1.js
+++ src/components/Update_1.js
@@ -1,56 +1,67 @@
-import axios from 'axios'
-import React,{useState,useEffect} from 'react'
-import { API_URL } from '../Constants/Url'
-import Read_1 from './Read_1'
-import { useNavigate } from 'react-router-dom'
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../Constants/Url";
+import Read_1 from "./Read_1old";
+import { useNavigate } from "react-router-dom";
 
 function Update_1() {
+  const [names, setnames] = useState("");
+  const [Experiences, setExperiences] = useState("");
+  const [dojs, setDoJs] = useState("");
+  const [id, setId] = useState("");
 
-  const [name,setname]= useState("")
-  const [Experience,setExperience]=useState("")
-  const [doj,setDoJ]= useState("")
-  const [id,setId]= useState("")
-
-const navigate=useNavigate()
-  useEffect(()=>{
+  const navigate = useNavigate();
+  useEffect(() => {
     // console.log(localStorage.getItem('id'))
     // console.log(localStorage.getItem('name'))
     // console.log(localStorage.getItem('Experience'))
     // console.log(localStorage.getItem('doj'))
 
+    //getting the stored values
+    setId(localStorage.getItem("id"));
+    setnames(localStorage.getItem("names"));
+    setExperiences(localStorage.getItem("Experiences"));
+    setDoJs(localStorage.getItem("dojs"));
+  }, []);
 
-    //getting the stored values 
-    setId(localStorage.getItem('id'))
-    setname(localStorage.getItem('name'))
-    setExperience(localStorage.getItem('Experience'))
-    setDoJ(localStorage.getItem('doj'))
-
-  },[])
-
-
-  
-  const handleSubmit1= async()=>{
+  const handleSubmit1 = async () => {
     //put method is used for updating
-await axios.put(`${API_URL}/${id}`,{name,Experience,doj})
+    await axios.put(`${API_URL}/${id}`, { names, Experiences, dojs });
 
-
-//after the updating the values, it navigate to the read page
-navigate('/read') 
-
-  }
-
+    //after the updating the values, it navigate to the read page
+    navigate("/read");
+  };
 
   return (
     <div>
-{/* update form */}
-    <input type="text" placeholder="Enetr Name" value={name} onChange={event=>setname(event.target.value)}></input><br></br>
+      {/* update form */}
+      <input
+        type="text"
+        placeholder="Enetr Name"
+        value={names}
+        onChange={(event) => setnames(event.target.value)}
+      ></input>
+      <br></br>
 
-   <input type="text" placeholder="Experience in years" value={Experience}  onChange={event=>setExperience(event.target.value)}></input><br></br>
-  <input type="text" placeholder="Enter the Date of Joining" value={doj}  onChange={event=>setDoJ(event.target.value)}></input><br></br>
-  <button type="button"onClick={handleSubmit1} className='updatebutton'>Update</button>
-  </div>
-  )
+      <input
+        type="text"
+        placeholder="Experience in years"
+        value={Experiences}
+        onChange={(event) => setExperiences(event.target.value)}
+      ></input>
+      <br></br>
+      <input
+        type="text"
+        placeholder="Enter the Date of Joining"
+        value={dojs}
+        onChange={(event) => setDoJs(event.target.value)}
+      ></input>
+      <br></br>
+      <button type="button" onClick={handleSubmit1} className="updatebutton">
+        Update
+      </button>
+    </div>
+  );
 }
 
-export default Update_1
+export default Update_1;
