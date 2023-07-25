6b60c95b82355a9adef4142bfbe83d6406ef4536
Divyadharshini999 <dharshini999dd@gmail.com>
2023-07-26 03:12:56 +0530

    sixth commit- create validation

--- src/components/Create_1.js
+++ src/components/Create_1.js
@@ -1,54 +1,81 @@
-import React from 'react'
-import {API_URL} from '../Constants/Url'
-import { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { API_URL } from "../Constants/Url";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  names: yup.string().required(),
+  Experiences: yup.number().integer().positive().required().min(1).max(99),
+  dojs: yup.string().required(),
+});
 
 function Create_1() {
+  const [names, setnames] = useState("");
+  const [Experiences, setExperiences] = useState("");
+  const [dojs, setDoJs] = useState("");
 
-  //useState 
-  const [name,setname]= useState("")
-  const [Experience,setExperience]=useState("")
-  const [doj,setDoJ]= useState("")
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
+  console.log(errors);
 
-  //for navigating create page to read page
-  const navigate= useNavigate()
+  const navigate1 = useNavigate();
 
-
-  //storing the input values in the API and it take some time to complete this function,
-  //basically,Javascript doen't not wait,it move to the next function 
-  // So we use ASYNC & AWAIT function to wait for that particular function.
-  const handleSubmit= async()=>{
-    //const handleSubmit=()=>{
-    // console.log(id)
-    // console.log(name)
-    // console.log(Experience)
-    // console.log(doj)
-    await axios.post(API_URL,{name,Experience,doj})
-
- 
-// after completing the function ,it navigate to read page.
-   navigate('/read')
-  }
-  
-  
   return (
-   
+    <form
+      className="inputContainer"
+      onSubmit={handleSubmit(async () => {
+        //console.log(data);
+        await axios.post(API_URL, { names, Experiences, dojs });
+        navigate1("/read");
+      })}
+    >
+      {/* for changing or to type something in the textbox, wehave to use "onChange" event function */}
 
-   
- <div className="inputContainer">
-           
-   {/* for changing or to type something in the textbox, wehave to use "onChange" event function */}
-            <input type="text" placeholder="Enetr Name" value={name} onChange={event=>setname(event.target.value)}></input><br></br>
+      <input
+        {...register("names")}
+        type="text"
+        placeholder="Enetr Name"
+        value={names}
+        onChange={(event) => setnames(event.target.value)}
+      ></input>
 
-            <input type="text" placeholder="Experience in years" value={Experience}  onChange={event=>setExperience(event.target.value)}></input><br></br>
-            <input type="text" placeholder="Enter the Date of Joining" value={doj}  onChange={event=>setDoJ(event.target.value)}></input><br></br>
-            <button type="submit"  className="addbutton"onClick={handleSubmit}>ADD</button>
-             
-     </div>
-     )
-    
+      <div className="error">{errors.names?.message} </div>
+      <br></br>
 
+      <input
+        {...register("Experiences")}
+        type="text"
+        placeholder="Experience in years"
+        value={Experiences}
+        onChange={(event) => setExperiences(event.target.value)}
+      ></input>
+      <div className="error">{errors.Experiences?.message} </div>
+      <br></br>
+
+      <input
+        {...register("dojs")}
+        type="date"
+        className="date"
+        placeholder="Enter the Date of Joining"
+        value={dojs}
+        onChange={(event) => setDoJs(event.target.value)}
+      ></input>
+      <div className="error">{errors.dojs?.message} </div>
+      <br></br>
+      <br></br>
+      <button type="submit" className="addbutton" onClick={handleSubmit}>
+        ADD
+      </button>
+    </form>
+  );
 }
-export default Create_1
+export default Create_1;
